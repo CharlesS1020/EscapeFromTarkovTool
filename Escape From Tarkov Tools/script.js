44f8ac0b36984d3ad9c9169fa3245ca36aa07955
CharlesS1020 <57678310+CharlesS1020@users.noreply.github.com>
2019-11-24 17:59:23 -0800

    Add files via upload

---
+++ Escape From Tarkov Tools/script.js
@@ -0,0 +1,235 @@
+// Curency
+var R = 'Roubles';
+var USD = 'USD';
+var E = 'Euros';
+
+// Hardware Items
+var analogThermometer = 'analogThermometer';
+var aPackOfNails = 'A Pack of Nails';
+var bolts = 'Bolts';
+var corrugatedHose = 'Corrugated Hose';
+var ductTape = 'Duct tape';
+var electricDrill = 'Electric drill';
+var electricMotor = 'Electric motor';
+var heatExchangeAkali = 'Heat-exchange alkali';
+var leathermanMultitool = 'Leatherman multitool';
+var lightBulb = 'Light bulb';
+var pressureGauge = 'Pressure gauge';
+var radiatorHelix = 'Radiator helix';
+var screwNut = 'Screw nut';
+var aPackOfScrews = 'A pack of screws';
+var aSetOfTools = 'A set of tools';
+var siliconeTube = 'Silicone tube';
+var wrench = 'Wrench';
+var elitePliers = 'Elite Pliers';
+var fireKleanGunLube = 'FireKlean gun lube';
+var sparkPlug = 'Spark plug';
+var xenomorphSealingFoam = 'Xenomorph sealing foam';
+var handDrill = 'Hand drill';
+var wd40 = 'WD-40 100ml';
+var shustriloSealingFoam = 'Shustrilo sealing foam';
+
+// Electronics
+var cpuFan = 'CPU fan';
+var capacitors = 'Capacitors';
+var carBattery = 'Car battery';
+var damagedHardDrive = 'Damaged hard drive';
+var phaseControlRelay = 'Phase control relay';
+var powerSupplyUnit = 'Power supply unit';
+var powercord = 'Powercord';
+var printedCircuitBoard = 'Printed circuit board';
+var ssdDrive = 'SSD drive';
+var secureFlashDrive = 'Secure flash drive';
+var wires = 'Wires';
+var workingLCD = 'Working LCD';
+var nixxorLens = 'NIXXOR lens';
+var militaryCOFDM = 'Military COFDM';
+var vpxFlashStorageModule = 'VPX Flash Storage Module';
+var gasAnalyzer = 'Gas analyzer';
+var militaryCable = 'Military cable';
+
+// Medical Items
+var medicalBloodset = 'Medical bloodset';
+var salineSolution = 'Saline solution';
+var sodiumBicarbonate = 'Sodium bicarbonate';
+var coffeeMajaica = 'Coffee Majaica';
+var ledx = 'LEDX';
+
+// Valuables
+var bronzeLion = 'Bronze lion';
+var intelligence = 'Folder with intelligence';
+var goldSkullRing = 'Gold skull ring';
+var goldenNeckChain = 'Golden neck chain';
+var roler = 'Roler';
+
+/* 
+// Totals
+
+// Hardware Items
+analogThermometer = 2;
+aPackOfNails = 4;
+bolts = 18;
+corrugatedHose = 26;
+ductTape = 6;
+electricDrill = 6;
+electricMotor = 9;
+heatExchangeAkali = 2;
+leathermanMultitool = 1;
+lightBulb = 14;
+pressureGauge = 6;
+radiatorHelix = 8;
+screwNut = 12;
+aPackOfScrews = 20;
+aSetOfTools = 6;
+siliconeTube = 14;
+wrench = 4;
+elitePliers = 4;
+fireKleanGunLube = 1;
+sparkPlug = 1;
+xenomorphSealingFoam = 3;
+handDrill = 1;
+wd40 = 4;
+shustriloSealingFoam = 5;
+
+// Electronics
+cpuFan = 50;
+capacitors = 12;
+carBattery = 4;
+damagedHardDrive = 4;
+phaseControlRelay = 16;
+powerSupplyUnit = 15;
+powercord = 13;
+printedCircuitBoard = 8;
+ssdDrive = 1;
+secureFlashDrive = 3;
+wires = 47;
+workingLCD = 2;
+nixxorLens = 8;
+militaryCOFDM = ;
+vpxFlashStorageModule = 2;
+gasAnalyzer = 3;
+militaryCable = 4;
+
+// Medical
+medicalBloodset = 1;
+salineSolution = 8;
+sodiumBicarbonate = 3;
+coffeeMajaica = 3;
+ledx = 1;
+
+// Valuables
+bronzeLion = 2;
+intelligence = 4;
+goldSkullRing = 2;
+goldenNeckChain = 6;
+roler = 3;
+*/
+
+var hideout = 'Hideout';
+
+var airFilteringUnit = 10000(USD);
+
+var bitcoinFarm = 10(cpuFan) + 5(powerSupplyUnit) +  5(powerSupplyUnit) +  1(electricDrill);
+var bitcoinFarm2 = 15(cpuFan) + 5(powerSupplyUnit) + 5(printedCircuitBoard) + 2(phaseControlRelay);
+var bitcoinFarm3 = 25(cpuFan) + 10(siliconeTube) + 1(electricMotor) + 2(pressureGauge);
+
+var boozeGenerator = 4(siliconeTube) + 2(analogThermometer) + 2(pressuregauge) + 5 (corrugatedHose)
+
+var generator = 100000(R);
+var genartor2 = 5(phaseControlRelay) + 1(electricMotor) + 15(wires);
+var generator3 = 6(phaseControlRelay) + 2(electricMotor) + 5(sparkPlug) + 5(powerSupplyUnit);
+
+var heating = 25000(R);
+var heating2 = 50000(R);
+var heating3 = 8(radiatorHelix) + 8(wires);
+
+var illumination = 
+var illumination2 = 
+var illumination3 = 
+
+var intelligenceCenter = 
+var intelligenceCenter2 = 
+var intelligenceCenter3 = 
+
+var lavatory = 
+var lavatory2 = 
+var lavatrory3 = 
+
+var library = 
+
+var medstation = 
+
+var nutritionUnit =
+var nutritionUnit2 = 
+var nutritionUnit3 = 
+
+var restSpace = 
+var restSpace2 = 
+var restSpace3 = 
+var scavCase = 
+var security = 
+var secuirty2 = 
+var security3 = 
+
+var shootingRange = 
+
+var solarPower = 
+
+var stash = 
+var stash2 = 
+var stash3 = 
+
+var vents = 
+var vents2 = 
+var vents3 = 
+
+var waterCollector = 
+var waterCollector2 = 
+var waterCollector3 = 
+
+var workbench = 
+var workbench2 = 
+var workbench3 = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
